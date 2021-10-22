0a11e0158eb0da53c81180ceb7beb460b1c969f4
Leandro Avram <leoavram7@gmail.com>
2021-10-22 09:27:51 -0300

    table websocket

--- src/server.js
+++ src/server.js
@@ -2,12 +2,30 @@ const express = require('express')
 const configRoutes = require('./router/routes')
 const exphbs = require('express-handlebars')
 const FileArray = require('./Contenedor')
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
 
 const fa = new FileArray('./productos.txt')
 
 const app = express()
 app.use(express.json())
+app.use('/', express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+
+io.on('connection', async socket => {
+  console.log('Nuevo cliente conectado')
+
+  socket.emit('actualizarProductos', await fa.getAll())
+
+  socket.on('nuevoProducto', async producto => {
+      await fa.save(producto)
+      io.sockets.emit('actualizarProductos', await fa.getAll())
+  })
+
+})
 
 app.engine('hbs', exphbs({
     extname: 'hbs',
@@ -17,7 +35,7 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.render('productform',{})
+    res.render('main',{})
   })
 
 app.get('/product', async (req, res) => {
@@ -35,7 +53,7 @@ configRoutes(app)
 
 /* Server Listen */
 const PORT = 8080
-const server = app.listen(PORT, () => {
+const server = httpServer.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${server.address().port}`)
 })
 server.on('error', error => console.log(`Error en servidor ${error}`))
